8215c3f6019a66f82afef20e1b45cf87a20fb7f6
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-03-27 23:31:52 +0100

    Add schema definition method for Chicago crime dataset

--- src/main/scala/org/codecraftlabs/spark/util/SchemaDefinition.scala
+++ src/main/scala/org/codecraftlabs/spark/util/SchemaDefinition.scala
@@ -1,14 +1,30 @@
 package org.codecraftlabs.spark.util
 
-import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{BooleanType, IntegerType, LongType, StringType, StructField, StructType}
 
 object SchemaDefinition {
   def chicagoCrimeDatasetSchemaDefinition(): StructType = {
     StructType(Array(
-      StructField("Id", LongType, nullable = false),
-      StructField("CaseNumber", StringType, nullable = false),
-      StructField("Date", StringType, nullable = false),
-      StructField("Block", StringType, nullable = false),
+      StructField("id", LongType, nullable = false),
+      StructField("caseNumber", StringType, nullable = false),
+      StructField("date", StringType, nullable = false),
+      StructField("block", StringType, nullable = false),
+      StructField("iucr", StringType, nullable = false),
+      StructField("primaryType", StringType, nullable = false),
+      StructField("description", StringType, nullable = false),
+      StructField("arrest", BooleanType, nullable = false),
+      StructField("domestic", BooleanType, nullable = false),
+      StructField("district", StringType, nullable = false),
+      StructField("ward", StringType, nullable = false),
+      StructField("communityArea", StringType, nullable = true),
+      StructField("fbiCode", StringType, nullable = true),
+      StructField("xCoordinate", LongType, nullable = true),
+      StructField("yCoordinate", LongType, nullable = true),
+      StructField("year", IntegerType, nullable = true),
+      StructField("updatedOn", StringType, nullable = true),
+      StructField("latitude", StringType, nullable = true),
+      StructField("longitude", StringType, nullable = true),
+      StructField("location", StringType, nullable = true)
     ))
   }
 }
