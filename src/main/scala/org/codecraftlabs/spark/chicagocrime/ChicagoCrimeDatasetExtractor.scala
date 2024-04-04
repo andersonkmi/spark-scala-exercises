852cc7c7121d728571247a82d91360b8e0b30a59
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-04-04 22:22:58 +0200

    Changes

---
+++ src/main/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDatasetExtractor.scala
@@ -0,0 +1,19 @@
+package org.codecraftlabs.spark.chicagocrime
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.codecraftlabs.spark.util.SchemaDefinition.chicagoCrimeDatasetSchemaDefinition
+
+class ChicagoCrimeDatasetExtractor {
+  def extractInitialDataset(spark: SparkSession, inputFolder: String): DataFrame = {
+    val schemaDefinition = chicagoCrimeDatasetSchemaDefinition()
+    val df = spark.read.format("csv").option("header", "true").schema(schemaDefinition).load(inputFolder)
+    df.printSchema()
+    df.select("id",
+      "caseNumber",
+      "date",
+      "block",
+      "primaryType",
+      "description",
+      "locationDescription")
+  }
+}
--- src/main/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDatasetProcessor.scala
+++ src/main/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDatasetProcessor.scala
@@ -1,10 +1,9 @@
-package org.codecraftlabs.spark
+package org.codecraftlabs.spark.chicagocrime
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.codecraftlabs.spark.util.SchemaDefinition.chicagoCrimeDatasetSchemaDefinition
 
-object ChicagoCrimeDatasetExtractor {
+object ChicagoCrimeDatasetProcessor {
   @transient private lazy val logger: Logger = Logger.getLogger("ChicagoCrimeDatasetExtractor")
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
@@ -26,24 +25,9 @@ object ChicagoCrimeDatasetExtractor {
     val outputFolder = args(1)
     logger.info(s"Input folder provided: '$inputFolder'")
 
-    // Defines the schema of the dataset
-    logger.info("Load schema definition")
-    val schemaDefinition = chicagoCrimeDatasetSchemaDefinition()
-
-    // Reads the file(s)
-    val df = spark.read.format("csv").option("header", "true").schema(schemaDefinition).load(inputFolder)
-    df.printSchema()
-    val rowCount = df.count()
-    logger.info(s"Total number of rows '$rowCount'")
-
-    // Extracts some columns: id, case number, date, block, primary type, description, location description, year
-    val extractedDF = df.select("id",
-      "caseNumber",
-      "date",
-      "block",
-      "primaryType",
-      "description",
-      "locationDescription")
+    // Executing first step
+    val chicagoCrimeDatasetExtractor = new ChicagoCrimeDatasetExtractor
+    val extractedDF = chicagoCrimeDatasetExtractor.extractInitialDataset(spark, inputFolder)
     val primaryTypeDF = extractedDF.select("primaryType").distinct()
 
     // Writes the current dataframe back
