d2d929b38b048b1d0831dcd7ddc48b3f6d36233d
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-04-06 09:09:45 +0200

    Add first valid unit test

--- src/test/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDataExtractorSpec.scala
+++ src/test/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDataExtractorSpec.scala
@@ -1,8 +1,8 @@
 package org.codecraftlabs.spark.chicagocrime
 
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
@@ -20,21 +20,30 @@ class ChicagoCrimeDataExtractorSpec extends AnyFlatSpec with Matchers with Befor
 
   private def createDataFrame(): DataFrame = {
     val sampleData = Seq(
-      Row("account001", "Mike", 1),
-      Row("account002", "Bob", 2),
-      Row("account003", "Bob", 2)
+      Row(11646166L, "JC213529", "09/01/2018 12:01:00 AM", "082XX S INGLESIDE AVE", "0810", "THEFT", "OVER $500", "RESIDENCE", false, true),
+      Row(11645836L, "JC212333", "05/01/2016 12:25:00 AM", "055XX S ROCKWELL ST", "1153", "DECEPTIVE PRACTICE", "FINANCIAL IDENTITY THEFT OVER $ 300", "", false, true)
     )
-    val schema = schemaDefinition()
+    val schema = chicagoCrimeDatasetSchemaDefinition()
     sparkSession.get.createDataFrame(sparkSession.get.sparkContext.parallelize(sampleData), schema)
   }
 
-  private def schemaDefinition(): StructType = {
-    val accountName         = StructField("accountName", StringType, nullable = false)
-    val userName            = StructField("userName", StringType, nullable = false)
-    val userId              = StructField("userId", IntegerType, nullable = false)
-
-    val fields = List(accountName, userName, userId)
-    StructType(fields)
+  "When setting up the raw dataframe" must "return a valid dataframe" in {
+    val df = createDataFrame()
+    df.count() mustEqual 2
   }
 
+  private def chicagoCrimeDatasetSchemaDefinition(): StructType = {
+    StructType(Array(
+      StructField("id", LongType, nullable = false),
+      StructField("caseNumber", StringType, nullable = false),
+      StructField("date", StringType, nullable = false),
+      StructField("block", StringType, nullable = false),
+      StructField("iucr", StringType, nullable = false),
+      StructField("primaryType", StringType, nullable = false),
+      StructField("description", StringType, nullable = false),
+      StructField("locationDescription", StringType, nullable = false),
+      StructField("arrest", BooleanType, nullable = false),
+      StructField("domestic", BooleanType, nullable = false)
+    ))
+  }
 }
