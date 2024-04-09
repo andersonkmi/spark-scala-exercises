5b434967c384df32546db62c9aabde7cbe0986b4
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-04-09 08:40:12 +0200

    Refactoring to remove some duplication

--- src/main/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDatasetProcessor.scala
+++ src/main/scala/org/codecraftlabs/spark/chicagocrime/ChicagoCrimeDatasetProcessor.scala
@@ -1,7 +1,7 @@
 package org.codecraftlabs.spark.chicagocrime
 
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.codecraftlabs.spark.util.SchemaDefinition.chicagoCrimeDatasetSchemaDefinition
 
 object ChicagoCrimeDatasetProcessor {
@@ -10,6 +10,9 @@ object ChicagoCrimeDatasetProcessor {
   private val Header: String = "header"
   private val True:String = "true"
   private val Overwrite: String = "overwrite"
+  private val CrimeCountPerPrimaryTypeFolder = "crime_count_per_primary_type"
+  private val CrimeCountPerPrimaryTypeYearMonthFolder = "crime_count_per_primary_type_year_month"
+
   private val chicagoCrimeDatasetExtractor = new ChicagoCrimeDatasetExtractor
 
   def main(args: Array[String]): Unit = {
@@ -44,12 +47,16 @@ object ChicagoCrimeDatasetProcessor {
 
     // Number of crime per primary type
     val crimeCountPerPrimaryType = chicagoCrimeDatasetExtractor.countCrimeGroupedByColumn(extractedDF, "primaryType")
-    crimeCountPerPrimaryType.write.format(Csv).option(Header, True).mode(Overwrite).save(s"$outputFolder/crime_count_per_primary_type")
+    saveDataFrameToCsv(crimeCountPerPrimaryType, s"$outputFolder/$CrimeCountPerPrimaryTypeFolder")
 
     // Group crime count per year, month, primaryType
     val dfWithYearMonth = chicagoCrimeDatasetExtractor.addYearAndMonthColumns(dfWithTimestamp)
     val crimeCountGroupedByYearMonthPrimaryType = chicagoCrimeDatasetExtractor.countCrimeGroupedByTypeYearMonth(dfWithYearMonth)
     val filteredResultsDF = chicagoCrimeDatasetExtractor.dropItemsWithoutYear(crimeCountGroupedByYearMonthPrimaryType)
-    filteredResultsDF.write.format(Csv).option(Header, True).mode(Overwrite).save(s"$outputFolder/crime_count_per_primary_type_year_month")
+    saveDataFrameToCsv(filteredResultsDF, s"$outputFolder/$CrimeCountPerPrimaryTypeYearMonthFolder")
+  }
+
+  private def saveDataFrameToCsv(df: DataFrame, destination: String): Unit = {
+    df.write.format(Csv).option(Header, True).mode(Overwrite).save(destination)
   }
 }
